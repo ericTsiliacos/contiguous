4ce808fb42382d9906836c9acc31241c879c1b03
Eric Tsiliacos and Wiley Kestner <pair+etsiliacos+wkestner@pivotal.io>
2017-01-03 16:08:11 -0800

    Add second test for single element array

--- contiguous.py
+++ contiguous.py
@@ -5,10 +5,12 @@ import unittest
 def subarray(array):
     return 0
 
-
 if __name__ == "__main__":
     class TestSubarray(unittest.TestCase):
         def test_empty_array(self):
             self.assertEqual(subarray([]), 0)
+
+        def test_single_item_array(self):
+            self.assertEqual(subarray([1]), 0)
 
     unittest.main()
