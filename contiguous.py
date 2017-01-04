e051b6ec892a009e1f824bd552bde7a1143f66b5
Eric Tsiliacos and Wiley Kestner <pair+etsiliacos+wkestner@pivotal.io>
2017-01-03 16:09:21 -0800

    Add failing test for smallest possible contiguous subarray (2 contiguous elements)

--- contiguous.py
+++ contiguous.py
@@ -13,4 +13,7 @@ if __name__ == "__main__":
         def test_single_item_array(self):
             self.assertEqual(subarray([1]), 0)
 
+        def test_2_ordered(self):
+            self.assertEqual(subarray([1, 2]), 2)
+
     unittest.main()
