29c2ee0ac79621bc834e3311198621c35370a16f
Eric Tsiliacos and Wiley Kestner <pair+etsiliacos+wkestner@pivotal.io>
2017-01-04 15:54:31 -0800

    Refactor to use builtin max, min, and len functions

--- contiguous.py
+++ contiguous.py
@@ -4,24 +4,15 @@ import unittest
 import sys
 
 def subarray(array):
-    contiguous_subarray_length = 0
-    maxiumum = 0
-    minimum = sys.maxsize
-    while array[contiguous_subarray_length:] != []:
-        current_element = array[contiguous_subarray_length]
-        maxiumum = current_element if current_element > maxiumum else maxiumum
-        minimum = current_element if current_element < minimum else minimum
-        contiguous_subarray_length += 1
-    if contiguous_subarray_length == 0:
-        return 0
-    if maxiumum - minimum == contiguous_subarray_length - 1:
-        return contiguous_subarray_length
+    array_length = len(array)
 
-    largest_subarray_from_start = subarray(array[:-1])
-    largest_subarray_from_end = subarray(array[1:])
-    if largest_subarray_from_start > largest_subarray_from_end:
-        return largest_subarray_from_start
-    return largest_subarray_from_end
+    if array_length == 0:
+        return 0
+
+    if max(array) - min(array) == array_length - 1:
+        return array_length
+
+    return max(subarray(array[:-1]), subarray(array[1:]))
 
 if __name__ == "__main__":
     class TestSubarray(unittest.TestCase):
