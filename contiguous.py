c4cdf1c1a243b4cfbae760270e288b122b31a6b6
Eric Tsiliacos and Wiley Kestner <pair+etsiliacos+wkestner@pivotal.io>
2017-01-04 15:35:35 -0800

    Pass test with transformations:
    - statement -> statements
    - unconditional -> if

--- contiguous.py
+++ contiguous.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 import unittest
+import sys
 
 def subarray(array):
     contiguous_subarray_length = 0
+    maxiumum = 0
+    minimum = sys.maxsize
     while array[contiguous_subarray_length:] != []:
+        current_element = array[contiguous_subarray_length]
+        maxiumum = current_element if current_element > maxiumum else maxiumum
+        minimum = current_element if current_element < minimum else minimum
         contiguous_subarray_length += 1
-    return contiguous_subarray_length
+    if contiguous_subarray_length == 0:
+        return 0
+    if maxiumum - minimum == contiguous_subarray_length - 1:
+        return contiguous_subarray_length
+
+    return subarray(array[:-1])
 
 if __name__ == "__main__":
     class TestSubarray(unittest.TestCase):
